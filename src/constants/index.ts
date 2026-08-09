55cd1d8d16d2ca24f43cd29b4e34f2cbb6de5238
ehsanKey <ehsanKeyhani82@gmail.com>
2026-08-09 11:18:11 +0330

    feat(auth): add auth provider context and useAuth hook

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 export { BREAKPOINTS } from './design';
 export { ROUTES } from './routes';
 export { AUTH_COOKIE_NAME } from './auth';
+export { QUERY_KEYS } from './query-keys';
 export {
   DEFAULT_ICON_SIZE,
   DEFAULT_ICON_STROKE_WIDTH,
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,1 +1,2 @@
 export { default as AppProviders } from './app-providers';
+export { default as AuthProvider } from './auth-provider';
